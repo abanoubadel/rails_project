10ca2dda62bd74be31c5f122204f6af822c36236
mohamedelkadi <molhm66@gmail.com>
2016-04-28 21:31:41 +0200

    add edit and show single order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -49,7 +49,7 @@ class OrdersController < ApplicationController
     invited_users << User.find_by_name(params.fetch('users'));
     order.users << invited_users
     if order.save
-      Notification.create(order,current_user,invited_users)
+      Notification.create(order, current_user, invited_users)
       redirect_to :back
     else
       redirect_to root_path
@@ -60,7 +60,10 @@ class OrdersController < ApplicationController
   # PATCH/PUT /orders/1.json
   def update
     respond_to do |format|
-      if @order.update(order_params)
+      @order.description = order_params['description']
+      @order.meal = order_params['meal'].to_i
+      @order.items =Item.find order_params['items'].take_while {|i| i.to_i > 0 }
+      if @order.save
         format.html { redirect_to @order, notice: 'Order was successfully updated.' }
         format.json { render :show, status: :ok, location: @order }
       else
@@ -88,6 +91,6 @@ class OrdersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def order_params
-    params.fetch(:order, {})
+    params.fetch(:order)
   end
 end
