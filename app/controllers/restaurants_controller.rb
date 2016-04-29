c2eefeb5dcc2695284997c39aa4c8499a04bee2e
mohamedelkadi <molhm66@gmail.com>
2016-04-29 23:21:35 +0200

    refactor layout to yiled the content of the header and display notices everywhere

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,17 +1,26 @@
 class RestaurantsController < ApplicationController
   before_action :authenticate_user!
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @userList= current_user.followed_users.select("name").map {|u| u.name}
+    @userList= current_user.followed_users.select("name").map { |u| u.name }
     @restaurant = Restaurant.find(params[:id])
     @items = @restaurant.items.all
     @order = Order.new
   end
 
   def make_order
+  end
 
+  def join_order
+    order_id = params.fetch(:order_id)
+    @order = Order.find order_id
+    @restaurant = @order.restaurant
+    @items = @restaurant.items.all
+    flash[:notice]="add your items to order"
+    render 'restaurants/show'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
@@ -16,22 +16,25 @@ Rails.application.routes.draw do
   resources :groups
   resources :relationships, only: [:create, :destroy]
   root to: 'static_pages#index'
-  match '/help',                to: 'static_pages#help',      via: 'get'
-  match '/about',               to: 'static_pages#about',     via: 'get'
-  match '/contact',             to: 'static_pages#contact',   via: 'get'
-  match '/notifications/json',  to: 'notifications#list',     via: 'get'
-  match '/notifications',       to: 'notifications#index',    via: 'get'
-  match '/notifications',       to: 'notifications#update',   via: 'post'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/notifications/json', to: 'notifications#list', via: 'get'
+  match '/notifications', to: 'notifications#index', via: 'get'
+  match '/notifications', to: 'notifications#update', via: 'post'
 
   #####################resturants ########################
 
-  get 'restaurants' , to: 'restaurants#index' , as: 'restaurants_list'
-  get 'restaurants/:id' , to: 'restaurants#show' , as: 'restaurant'
-  post 'restaurants/:id' , to: 'orders#create' , as: 'make_order'
+  get 'restaurants', to: 'restaurants#index', as: 'restaurants_list'
+  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+  post 'restaurants/:id', to: 'orders#create', as: 'make_order'
+  get 'orders/restaurants/join/:order_id', to: 'restaurants#join_order', as: 'join_order'
+
   ###################### orders ############################
 
   resources :orders do
   end
+
   get 'invitations', to: 'orders#invitations'
 
 
